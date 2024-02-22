a1cc7b25e3d4e261653a075cdb0e14867d3a3faa
Sreeni <seenu619@gmail.com>
2024-02-22 11:45:32 +0530

    edit inst

--- application/views/institution/editinstitution.php
+++ application/views/institution/editinstitution.php
@@ -35,16 +35,11 @@
                             <div class="form-group">
                                 <label for="institution_name">Institution Name:</label>
                                 <input type="text" class="form-control" name="institution_name" id="institution_name"
-                                    value="<?php echo set_value('institution_name', $institution['institution_name']); ?>">
+                                    value="<?php echo set_value('institution_name', $institution['institution_name']); ?>"
+                                    readonly>
                                 <?=form_error('institution_name','<div class="text-danger">','</div>');?>
                             </div>
-                            <div class="form-group">
-                                <label for="institution_name_vernacular">Vernacular Institution Name:</label>
-                                <input type="text" class="form-control" name="institution_name_vernacular"
-                                    id="institution_name_vernacular"
-                                    value="<?php echo set_value('institution_name_vernacular', $institution['institution_name_vernacular']); ?>">
-                                <?=form_error('institution_name_vernacular','<div class="text-danger">','</div>');?>
-                            </div>
+
                             <div class="form-group">
                                 <label for="principal_name">Principal Name:</label>
                                 <input type="text" class="form-control" name="principal_name" id="principal_name"
@@ -70,98 +65,21 @@
                                     value="<?php echo set_value('principal_email', $institution['principal_email']); ?>">
                                 <?=form_error('principal_email','<div class="text-danger">','</div>');?>
                             </div>
-                            <div class="form-group">
-                                <label for="status">District Name:</label>
-                                <select name="district_id" id="district_id" class="form-control input-lg select2">
-                                    <option value="">Select Districts</option>
-                                    <?php
-                                $talu=$this->admin_model->getDetailsbyfield($institution['place_id'],'place_id',"places")->result(); 
-                                $taluk_id=$talu[0]->taluk_id;
 
-                                $bloc=$this->admin_model->getDetailsbyfield($taluk_id,'taluk_id',"taluks")->result(); 
-                                $block_id=$bloc[0]->block_id;
-
-                                $dist=$this->admin_model->getDetailsbyfield( $block_id,'block_id',"blocks")->result();
-                                $district_id=$dist[0]->district_id;
-                                foreach($districts as $row)
-                                {
-                                    $active=($district_id == $row['district_id']) ? "selected" :"";
-                                    echo '<option '.$active.' value="'.$row["district_id"].'">'.$row["district_name"].'</option>';
-                                }
-                                ?>
-                                </select>
-                                <?=form_error('district_id','<div class="text-danger">','</div>');?>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-success btn-sm">Update</button>
+                                <a href="<?=base_url();?>institution/institutions"
+                                    class="btn btn-dark btn-sm float-right" role="button">Cancel</a>
                             </div>
-                            <div class="form-group">
-                                <label for="status">Block Name:</label>
-                                <select name="block_id" id="block_id" class="form-control input-lg select2">
-                                    <option value="">Select Block</option>
-                                    <?php
-                                foreach($blocks as $row)
-                                {
-                                    $active=( $block_id == $row["block_id"]) ? "selected" :"";
-                                    echo '<option '.$active.' value="'.$row["block_id"].'">'.$row["block_name"].'</option>';
-                                }
-                                ?>
-                                </select>
-                                <?=form_error('block_id','<div class="text-danger">','</div>');?>
-                            </div>
-                            <div class="form-group">
-                                <label for="status">Taluk Name:</label>
-                                <select name="taluk_id" id="taluk_id" class="form-control input-lg select2">
-                                    <option value="">Select Taluk</option>
-                                    <?php
-                                foreach($taluks as $row)
-                                {
-                                    $active=( $taluk_id == $row["taluk_id"]) ? "selected" :"";
-                                    echo '<option '.$active.' value="'.$row["taluk_id"].'">'.$row["taluk_name"].'</option>';
-                                }
-                                ?>
-                                </select>
-                                <?=form_error('taluk_id','<div class="text-danger">','</div>');?>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="status">Place Name:</label>
-                                <select name="place_id" id="place_id" class="form-control input-lg select2">
-                                    <option value="">Select Place</option>
-                                    <?php
-                                foreach($places as $row)
-                                {
-                                    $active=($institution['place_id'] == $row['place_id']) ? "selected" :"";
-                                    echo '<option '.$active.' value="'.$row["place_id"].'">'.$row["place_name"].'</option>';
-                                }
-                                ?>
-                                </select>
-                                <?=form_error('place_id','<div class="text-danger">','</div>');?>
-                            </div>
+                            <?php echo form_close(); ?>
                         </div>
-
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-success btn-sm">Update</button>
-                            <a href="<?=base_url();?>institution/institutions" class="btn btn-dark btn-sm float-right"
-                                role="button">Cancel</a>
-                        </div>
-                        <?php echo form_close(); ?>
                     </div>
-
-
                 </div>
-
-
-
-
-
-
-
-
-
+                <!-- /.col -->
             </div>
-            <!-- /.col -->
+            <!-- /.row -->
         </div>
-        <!-- /.row -->
-</div>
-<!-- /.container-fluid -->
-</section>
-<!-- /.content -->
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
